85626bec82257727ec3dfc69815d528d3b9c3a4a
Julio Correa <julio.correa@takecommandhealth.com>
2024-01-09 09:26:19 -0600

    Generate Document and CommonCharacters issues second solution

--- src/com/code/challenge/strings/GenerateDocument.java
+++ src/com/code/challenge/strings/GenerateDocument.java
@@ -2,16 +2,25 @@ package com.code.challenge.strings;
 
 import org.assertj.core.api.Assertions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GenerateDocument {
 
     public static void main(String[] args) {
         String characters = "Bste!hetsi ogEAxpelrt x ";
         String document = "AlgoExpert is the Best!";
         boolean expected = true;
-        boolean actual = generateDocument(characters, document);
+        boolean actual = generateDocumentTwo(characters, document);
         Assertions.assertThat(expected == actual);
     }
 
+    /**
+     * runs in Big 0(m + (n + m) time and Big O(1) space
+     * @param characters
+     * @param document
+     * @return
+     */
     public static boolean generateDocument(String characters, String document) {
         // Write your code here.
         for (int i = 0; i < document.length(); i++) {
@@ -28,6 +37,40 @@ public class GenerateDocument {
             }
             if(countInCharacters < countInDocument )
                 return false;
+        }
+        return true;
+    }
+
+    /**
+     * runs in Big O(c(stands for uniques character * (n+m)) time Big O(1) space
+     * @param characters
+     * @param document
+     * @return
+     */
+    public static boolean generateDocumentTwo(String characters, String document) {
+        // Write your code here.
+        for(int i=0; i < document.length(); i++) {
+            Set<Character> counted = new HashSet<>();
+            int charactersCount = 0;
+            int documentCount = 0;
+            char c = document.charAt(i);
+            if(!counted.contains(c)) {
+                counted.add(c);
+
+                for(int j =0; j < characters.length(); j++) {
+                    if(characters.charAt(j) == c)
+                        charactersCount++;
+                }
+
+                for(int k =0; k < document.length(); k++) {
+                    if(document.charAt(k) == c)
+                        documentCount++;
+                }
+            }
+
+            if(charactersCount < documentCount)
+                return false;
+
         }
         return true;
     }
