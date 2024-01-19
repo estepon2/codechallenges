d0e8828a92ca10d9bedef95dbf6553693aa5c7af
Julio Correa <julio.correa@takecommandhealth.com>
2024-01-19 12:20:37 -0600

    First Non Repeating Character solution

---
+++ src/com/code/challenge/strings/FirstNonRepeatingCharacter.java
@@ -0,0 +1,23 @@
+package com.code.challenge.strings;
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+    }
+
+    // brute force solution
+    public static int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        for(int i= 0; i< string.length(); i ++) {
+            boolean duplicate = false;
+            for(int j=0; j < string.length(); j++) {
+                if(string.charAt(i) == string.charAt(j) && i != j)
+                    duplicate = true;
+            }
+            if(!duplicate)
+                return i;
+        }
+        return -1;
+    }
+}
