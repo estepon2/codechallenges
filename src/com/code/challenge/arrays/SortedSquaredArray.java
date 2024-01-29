a36d2fa808b24ca971cd26b1c9cd22c37bcb1088
Julio Correa <julio.correa@takecommandhealth.com>
2024-01-29 15:03:39 -0600

    SortedSquaredArray second solution solution

--- src/com/code/challenge/arrays/SortedSquaredArray.java
+++ src/com/code/challenge/arrays/SortedSquaredArray.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 public class SortedSquaredArray {
 
     public static void main(String[] args) {
-        int[] input = new int[] {1, 2, 3, 5, 6, 8, 9};
-        int[] expected = new int[] {1, 4, 9, 25, 36, 64, 81};
-        int[] actual = sortedSquaredArray(input);
+        int[] input = new int[]{1, 2, 3, 5, 6, 8, 9};
+        int[] expected = new int[]{1, 4, 9, 25, 36, 64, 81};
+        int[] actual = sortedSquaredArrayTwo(input);
         for (int i = 0; i < expected.length; i++) {
             Assertions.assertThat(expected[i]).isEqualTo(actual[i]);
         }
@@ -19,12 +19,38 @@ public class SortedSquaredArray {
     public static int[] sortedSquaredArray(int[] array) {
         int[] squaredArray = new int[array.length];
 
-        for(int i=0; i < array.length; i++ ) {
+        for (int i = 0; i < array.length; i++) {
             squaredArray[i] = array[i] * array[i];
         }
 
         Arrays.sort(squaredArray);
 
         return squaredArray;
+    }
+
+    //Big O(n) space Big O(n) time
+    public static int[] sortedSquaredArrayTwo(int[] array) {
+        int[] sortedArray = new int[array.length];
+        int leftIndex = 0;
+        int rightIndex = array.length - 1;
+        int indexSortedArray = array.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int absLeft = Math.abs(array[leftIndex]);
+            int absRight = Math.abs(array[rightIndex]);
+            if (absLeft > absRight) {
+                int squaredValue = absLeft * absLeft;
+                sortedArray[indexSortedArray] = squaredValue;
+                leftIndex++;
+                indexSortedArray--;
+            } else {
+                int squaredValue = absRight * absRight;
+                sortedArray[indexSortedArray] = squaredValue;
+                rightIndex--;
+                indexSortedArray--;
+
+            }
+        }
+        return sortedArray;
     }
 }
