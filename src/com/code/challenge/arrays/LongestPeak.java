ef5953f7149185d084d6561c493a1da18d0a73af
Julio Correa <julio.correa@takecommandhealth.com>
2024-03-06 08:38:07 -0600

    longestPeak solution

--- src/com/code/challenge/arrays/LongestPeak.java
+++ src/com/code/challenge/arrays/LongestPeak.java
@@ -21,23 +21,21 @@ public class LongestPeak {
         int i = 1;
 
         while(i < array.length -1){
-            boolean isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+            boolean isPeak = array[i-1] < array[i] && array[i+1] < array[i];
             if(!isPeak){
-                i += 1;
+                i++;
                 continue;
             }
 
             int leftIdx = i - 2;
-            while(leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]){
+            while(leftIdx >= 0 && array[leftIdx] < array[leftIdx+1])
                 leftIdx -= 1;
-            }
 
             int rightIdx = i + 2;
-            while(rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]){
+            while(rightIdx < array.length && array[rightIdx] < array[rightIdx -1])
                 rightIdx += 1;
-            }
 
-            int currentPeakLength = rightIdx - leftIdx - 1;
+            int currentPeakLength = rightIdx - leftIdx -1;
             if(currentPeakLength > longestPeakLength){
                 longestPeakLength = currentPeakLength;
             }
