a31f15b572aaaa8ca1ac420517f7082027760be6
Julio Correa <julio.correa@takecommandhealth.com>
2024-03-15 08:59:51 -0600

    arrayOfProducts first solution

--- src/com/code/challenge/arrays/ArrayOfProducts.java
+++ src/com/code/challenge/arrays/ArrayOfProducts.java
@@ -8,7 +8,7 @@ public interface ArrayOfProducts {
         int[] input = new int[]{5, 1, 4, 2};
         int[] expected = new int[]{8,40,10,20};
 
-        int[] output = arrayOfProducts(input);
+        int[] output = arrayOfProductsTwo(input);
 
         Assertions.assertThat(output).isEqualTo(expected);
 
@@ -30,6 +30,31 @@ public interface ArrayOfProducts {
             products[i] = product;
         }
 
+        return products;
+    }
+
+    // runs in Big O(n) time and Big O(n) space
+    static public int[] arrayOfProductsTwo(int[] array) {
+
+        int[] products = new int[array.length];
+        int[] leftProducts = new int[array.length];
+        int[] rightProducts = new int[array.length];
+
+        int leftRunningProduct = 1;
+        for( int i = 0; i < array.length; i++){
+            leftProducts[i] = leftRunningProduct;
+            leftRunningProduct *= array[i];
+        }
+
+        int rightRunningProduct = 1;
+        for(int i = array.length -1; i >=0; i--){
+            rightProducts[i] = rightRunningProduct;
+            rightRunningProduct *= array[i];
+        }
+
+        for(int i = 0; i < array.length; i ++){
+            products[i] = leftProducts[i] * rightProducts[i];
+        }
         return products;
     }
 }
